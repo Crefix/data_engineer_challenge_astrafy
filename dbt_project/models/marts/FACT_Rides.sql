e141c57a55c0bb8267ace3eac46f45d38e242443
Cris Navas Marcano <cris.navas@cognodata.com>
2025-01-22 20:29:52 +0100

    feat: update README and dbt models
    - Removed deprecated and example models
    - Synced with prod

---
+++ dbt_project/models/marts/FACT_Rides.sql
@@ -0,0 +1,37 @@
+{{ config(
+    materialized='incremental'
+) }}
+
+WITH new_data AS (
+    SELECT
+        taxi.unique_key,
+        taxi.trip_start_timestamp,
+        taxi.trip_end_timestamp,
+        taxi.trip_seconds,
+        taxi.trip_miles,
+        taxi.fare,
+        taxi.pickup_location,
+        taxi.dropoff_location,
+        taxi.payment_type,
+        taxi.tips,
+        weather.temperature,
+        weather.weather_main,
+        weather.weather_description,
+        weather.humidity,
+        weather.wind_speed,
+        weather.timestamp AS weather_timestamp
+    FROM {{ ref('stg_chicago_taxi') }} taxi
+    LEFT JOIN {{ ref('chicago_weather') }} weather
+    ON DATE(taxi.trip_start_timestamp) = DATE(weather.timestamp)
+    WHERE 
+        taxi.trip_start_timestamp BETWEEN '2023-06-01' AND '2023-12-31'
+)
+
+SELECT *
+FROM new_data
+{% if is_incremental() %}
+    WHERE unique_key NOT IN (
+        SELECT unique_key
+        FROM {{ this }}
+    )
+{% endif %}
--- dbt_project/models/staging/stg_weather_data.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    temperature,
-    humidity,
-    wind_speed,
-    weather_main,
-    weather_description,
-    timestamp
-FROM `tenacious-torus-447715-b4.Chicago_Taxi_Trips.chicago_weather`
